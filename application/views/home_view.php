daff6852352c640ef1d623cb1b9400d9f7627f36
tharindulucky <tharinduboy@gmail.com>
2016-11-01 16:58:58 +0530

    Public directory setup completed. Added utility_helper.php, Edited autoload.php in config folder, and created CSS file inside public older

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,1 +1,18 @@
-This is Home!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Welcome to my first CodeIgniter application.</title>
+<link href="<?php echo asset_url();?>css/styles.css" rel="stylesheet">
+</head>
+<body>
+<div id="container">
+    <h1>Welcome to Tharindu's  CodeIgniter Application.</h1>
+    <div id="body">
+        <div class="gallery">
+            
+        </div>
+    </div>
+</div>
+</body>
+</html>
