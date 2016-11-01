4fa08d86ae1c32c4ef703fcaeb0f1c9a054420fb
tharindulucky <tharinduboy@gmail.com>
2016-11-01 17:16:10 +0530

    Bootstrap CDN added

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -3,6 +3,9 @@
 <head>
 <meta charset="utf-8">
 <title>Welcome to my first CodeIgniter application.</title>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 <link href="<?php echo asset_url();?>css/styles.css" rel="stylesheet">
 </head>
 <body>
